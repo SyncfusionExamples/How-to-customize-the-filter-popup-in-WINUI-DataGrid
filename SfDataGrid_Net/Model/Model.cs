5f28835b6c2098c7a76c758250b9828c3b0017b7
ChidanandMurugaiah <chidanand.murugaiah@syncfusion.com>
2024-02-16 11:06:08 +0530

    ES-867511 Concerns addressed

--- SfDataGrid_Net/Model/Model.cs
+++ SfDataGrid_Net/Model/Model.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,30 +15,31 @@ namespace SfDataGrid_Net.Model
         string customerName;
         string shippingCity;
 
+        [Display(Name = "Order ID")]
         public int OrderID
         {
             get { return orderID; }
             set { orderID = value; }
         }
-
+        [Display(Name = "Customer ID")]
         public string CustomerID
         {
             get { return customerId; }
             set { customerId = value; }
         }
-
+        [Display(Name = "Customer Name")]
         public string CustomerName
         {
             get { return customerName; }
             set { customerName = value; }
         }
-
+        [Display(Name = "Country")]
         public string Country
         {
             get { return country; }
             set { country = value; }
         }
-
+        [Display(Name = "Ship City")]
         public string ShipCity
         {
             get { return shippingCity; }
